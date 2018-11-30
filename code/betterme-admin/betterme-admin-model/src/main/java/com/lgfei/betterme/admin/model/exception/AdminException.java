0266405fc42d18182e3a0d47c93da561a6c7ea0d
lgfei <longuofei@163.com>
2018-12-01 00:59:03 +0800

    init

---
+++ code/betterme-admin/betterme-admin-model/src/main/java/com/lgfei/betterme/admin/model/exception/AdminException.java
@@ -0,0 +1,8 @@
+package com.lgfei.betterme.admin.model.exception;
+
+import com.lgfei.betterme.framework.model.exception.BaseException;
+
+public class AdminException extends BaseException
+{
+    private static final long serialVersionUID = 8955414629302059034L;
+}
